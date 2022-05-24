1cd3227e754acd23244de35a870b5ed04edb0445
TomasUri <urinovic.tomas@gmail.com>
2022-05-24 11:26:25 +0300

    form

---
+++ vienaragiai/012/form.php
@@ -0,0 +1,38 @@
+<?php
+//scenarijus POST
+$_SERVER['REQUEST_METHOD'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    die($_POST['tek']);
+    header('Location: http://localhost/php-paskaitos/vienaragiai/012/form.php');
+    die;
+
+}
+
+// Scenarijus GET
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    
+    <form action = "" method = "post">
+
+    Tavo tekstas: <input type = "text" name = "tek">
+
+    <button type = "submit">GO</button>
+
+    </form>
+
+
+
+
+</body>
+</html>
